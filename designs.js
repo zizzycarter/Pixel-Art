ed63bb359b68706f601d0e4fd2f565e5a9b50d30
Zainab Ayodimeji <Zayodimeji@gmail.com>
2018-06-27 23:45:26 +0100

    Fix
    Bug fixes

--- designs.js
+++ designs.js
@@ -45,8 +45,6 @@ function makeGrid(height, width) {
     for (let j= 0; j < width; j++) {
         canvas_rows.append('<td></td>');
     }
-<<<<<<< HEAD
-=======
     //selects the cells and assign them to a variable
     let canvasCells = $('td');
     //Event listener that sets the background color of the cell to the user's selected color 
@@ -55,7 +53,6 @@ function makeGrid(height, width) {
         const color = $('#colorPicker').val();
         $(this).attr('bgcolor', color);
     })
->>>>>>> 2d8a54ba55146f5d2c15a3da6b525ce2a2e487e1
 }
 
 /**
